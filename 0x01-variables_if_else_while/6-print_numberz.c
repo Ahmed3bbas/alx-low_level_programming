90de2ee0b6c36d1c01631d6db5d203c4880b69e3
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-11 14:48:23 -0700

    Add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+#define NUM_SIZE  10
+/* betty style doc for function main goes there */
+/**
+ * main - My Main Program
+ *Return: Eroor or Success (0)
+*/
+int main(void)
+{
+/* your code goes there */
+int i = 0;
+for (i = 0; i < NUM_SIZE; i++)
+{
+putchar(( i + 48));
+}
+putchar('\n');
+return (0);
+}
