3fce0f2d3047a7e84900db83239025d424bf404f
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-11 14:24:00 -0700

    Add 4-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+#define ALPHA_SIZE  26
+/* betty style doc for function main goes there */
+/**
+ * main - My Main Program
+ *Return: Eroor or Success (0)
+*/
+int main(void)
+{
+char alpha[ALPHA_SIZE] = "abcdefghijklmnopqrstuvwxyz";
+/* your code goes there */
+int i = 0;
+for (i = 0; i < ALPHA_SIZE; i++)
+{
+if (alpha[i] == 'q' || alpha[i] == 'e')
+{
+continue;
+}
+putchar(alpha[i]);
+}
+putchar('\n');
+return (0);
+}
