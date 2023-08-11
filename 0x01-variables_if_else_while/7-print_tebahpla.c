029e3f54e62fd660dc67643be7b48d5f22edbdcb
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-11 14:56:30 -0700

    Add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+#define ALPHA_SIZE  26
+/* betty style doc for function main goes there */
+/**
+ * main - My Main Program
+ *Return: Eroor or Success (0)
+*/
+int main(void)
+{
+char alpha[ALPHA_SIZE] = "abcdefghijklmnopqrstuvwxyz";
+/* your code goes there */
+int i = 0;
+for (i =( ALPHA_SIZE - 1); i >= 0; i--)
+{
+putchar(alpha[i]);
+}
+putchar('\n');
+return (0);
+}
