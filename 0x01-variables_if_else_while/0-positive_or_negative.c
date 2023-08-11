4db800505182bc2003ff3d4c5f9174076e1c813b
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-11 13:02:16 -0700

    Add 0-positive_or_negative.c_Try4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,6 +6,7 @@
 /* betty style doc for function main goes there */
 /**
  * main -Main Program
+ *Return: Eroor or Success
 */
 int main(void)
 {
@@ -13,11 +14,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if(n > 0)
+if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if(n < 0)
+else if (n < 0)
 {
 printf("%d is negative\n", n);     
 }
