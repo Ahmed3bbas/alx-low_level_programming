73d873a8e752781d7dd9cb22d8fa9ad823eb3616
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-15 04:02:23 -0700

    add 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "_putchar.h"
+
+/**
+  * main - print string without printf
+  * void
+  * 
+  * Return: 0 for success state
+ */
+
+int main(void)
+{
+	char* str = "_putchar";
+	int i = 0;
+	char c = str[i];
+
+	while (c != '\0')
+	{
+		_putchar(c);
+		i++;
+		c = str[i];
+	}
+
+	_putchar('\n');
+	return(0);
+}
