b9f379f6431d8bf5cea40182ae697d93b8a211e5
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-15 05:50:58 -0700

    add: 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+  * _abs - get the absolute value of inger number
+  * @n: intger number
+  *
+  * Return: The absolute value of input number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-1 * n);
+	}
+}
