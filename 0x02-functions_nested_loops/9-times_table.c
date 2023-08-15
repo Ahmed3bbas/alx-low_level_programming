7e901ec52ebf9ba1e8272c460fd6c9f465ca43ff
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-15 07:20:12 -0700

    add: 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+  * times_table - 
+ */
+
+void times_table(void)
+{
+	int c = 0, r = 0, res;
+
+	while ( c < 10 )
+	{
+		r = 0;
+
+		while ( r < 10)
+		{
+			res = r * c;
+			if (res < 10)
+			{
+				if ( r != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(res + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+			if (r != 9)
+			{
+				_putchar(',');
+			}
+			r++;
+		}
+		_putchar('$');
+		_putchar('\n');
+
+		c++;
+	}
+}
