2795a91836e783cc05eaa01c2ff8c81179421460
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-15 08:41:02 -0700

    add: 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+  * add - sumation of two integer
+  * @n1: first intger number
+  * @n2: second intger number
+  *
+  * Return: the sum of 2 intgers
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
