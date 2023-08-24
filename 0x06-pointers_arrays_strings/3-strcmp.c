b010a6e7aadbfe3a96241049cfb54cb3eed6745f
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-24 14:13:43 -0700

    Add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,48 @@
+/**
+*_strcmp - Compare two strings
+*@s1: First String
+*@s2: Second Striing
+*
+*Return: int 0(s1 = s2), 1(s1 > s2), -1(s1 <s2)
+*/
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+int j = 0;
+int cmp = -1;
+int s1_len = 0;
+int s2_len = 0;
+while (*(s1 + i) != '\0')
+{
+i++;
+s1_len++;
+}
+while (*(s2 + j) != '\0')
+{
+j++;
+s2_len++;
+}
+if (s1_len < s2_len)
+{
+cmp = -15;
+}
+else if (s1_len > s2_len)
+{
+cmp = 15;
+}
+else
+{
+for (i = 0, j = 0; i < s2_len; i++)
+{
+if (s1[i] == s2[j])
+{
+j++;
+if (j == s2_len)
+{
+cmp = 0;
+}
+}
+}
+}
+return (cmp);
+}
