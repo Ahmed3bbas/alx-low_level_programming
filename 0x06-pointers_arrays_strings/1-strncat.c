5e6b534ee4fc1cdd1bdcd097928e4e4eafa1c994
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-24 09:21:00 -0700

    Add 1-srencat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * _strncat - Copy n Characters from src to dest
+ *@*dest: Pointer to the  First String to be concatenated
+ *@*src: Pointer to the Second String to be concatenated
+ *@n: Number of Charactrs to be Concatenated
+ *
+ *Return: Char*
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int src_len = 0;
+int dest_len = 0;
+int i = 0;
+int j = 0;
+while (*(src + i) != '\0')
+{
+i++;
+src_len++;
+}
+while (*(dest + j) != '\0')
+{
+j++;
+dest_len++;
+}
+if(src_len >= n)
+{
+for (i = 0; i < n; i++)
+{
+*(dest + dest_len + i) = *(src + i);
+}   
+}
+else
+{
+for (i = 0; i < src_len; i++)
+{
+*(dest + dest_len + i) = *(src + i);
+}     
+}
+return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define __MAIN__H
 
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 #endif
