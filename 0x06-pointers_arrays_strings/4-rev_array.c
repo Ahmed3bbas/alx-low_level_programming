ac626d5f2738dc3923b5920245ff69803fa09a4c
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-25 15:06:39 -0700

    Add 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * reverse_array - Revers Array
+ * @a: The array
+ * @n: The number of elements in the array
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+int *ptr_m = (int *)malloc(n * sizeof(int));
+int i = 0;
+int j = 0;
+for (i = 0, j = n - 1; i < n; i++, j--)
+{
+ptr_m[i] = a[j];
+}
+memcpy(a, ptr_m, n * 4);
+}
