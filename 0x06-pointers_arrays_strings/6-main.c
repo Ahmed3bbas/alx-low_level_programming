110b7a3511252f8e58308f67e70bc8db82f59d2d
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-08-25 18:51:50 -0700

    Fix 6-main.c

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -7,10 +7,21 @@
  */
 int main(void)
 {
+int i = 0;
 char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 char *ptr;
 ptr = cap_string(str);
-printf("%s", ptr);
-printf("%s", str);
+while(str[i] != '\0')
+{
+putchar(str[i]);
+i++;
+}
+i = 0;
+while(ptr[i] != '\0')
+{
+putchar(ptr[i]);
+i++;
+}
+
 return (0);
 }
