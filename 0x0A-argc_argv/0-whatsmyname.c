32ba17ef7102aadb983400a06a5150fdbf58b5dd
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-01 15:58:05 -0700

    Add 0-whatsmyname.c, 1-args.c,Betty-Style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code 
- *
+ *main - check the code 
+ *@argc: Number of Arguments
+ *@argv: Argument Array
  * Return: Always 0.
  */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 printf("%s\n", argv[argc - argc]);
 return (0);
