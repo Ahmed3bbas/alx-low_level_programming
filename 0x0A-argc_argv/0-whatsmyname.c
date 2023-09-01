fef2d231bd4a85db03384c306e241208e8c27d9c
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-01 15:14:32 -0700

    Add 0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char * argv[])
+{
+puts(argv[argc-1]);
+return (0);
+}
+
 
