6ff1ad65940daba015649f494dc0710614bd9dfa
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-01 15:56:06 -0700

    Add 1-args.c,Betty-Style

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code 
- *
+ * main - check the code
+ *@argc: Counter of Arguments
+ *@argv: Argument Array
  * Return: Always 0.
  */
-int main(int argc, __attribute__((__unused__))char * argv[])
+int main(int argc, __attribute__((__unused__))char *argv[])
 {
 int sum = 0;
 while (argc > 0)
@@ -14,7 +15,7 @@ while (argc > 0)
 sum++;
 argc--;
 }
-printf("%d\n", sum-1);
+printf("%d\n", sum - 1);
 return (0);
 }
 
