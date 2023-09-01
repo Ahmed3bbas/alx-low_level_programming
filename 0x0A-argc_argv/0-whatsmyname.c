4e6ba04d81af6cef942770f8b7a590aaa791cdd1
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-01 15:59:47 -0700

    Add 0-whatsmyname.c, 1-args.c,Betty-Style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  *main - check the code 
  *@argc: Number of Arguments
  *@argv: Argument Array
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - check the code
  *@argc: Counter of Arguments
  *@argv: Argument Array
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(int argc, __attribute__((__unused__))char *argv[])
 {
