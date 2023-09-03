8fa6a764ae59ce7bbdb52d24395affbeae3013aa
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-03 05:32:21 -0700

    add: 0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _printchar - print char
+  * @s: sting
+  * @i: index of char
+  * Return: void
+  */
+
+void _printchar(char *s, int i)
+{
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_printchar(s, ++i);
+}
+
+
+/**
+  * void _puts_recursion - print string
+  * @s: string
+  *
+  * Return: void
+  */
+void _puts_recursion(char *s)
+{
+	_printchar(s, 0);
+	
+}
