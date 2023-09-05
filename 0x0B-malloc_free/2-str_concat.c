71de65158d76028ffb25b25a0d947a3b08780a61
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-05 08:40:37 -0700

    fix 2: str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 	char *newstr;
 	int i, l1, l2;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	else if (s1 == NULL)
 		return (s2);
