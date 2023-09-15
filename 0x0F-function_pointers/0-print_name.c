fd27dffbea55f75d7040dbbd9a21c5cbeeb5f8d1
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-15 10:33:28 -0700

    Add 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ *print_name - Function that Prints Name
+ *@name: name of the person
+ *@f: Pointer to Function
+ * Return: Always 0.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
+
