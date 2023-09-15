8a975ac031e73c418c39ebafcb626acc263c2e14
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-15 13:49:28 -0700

    Edit in 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 /**
- *print_name - Function that Prints Name
- *@name: name of the person
- *@f: Pointer to Function
+ * print_name - Function that Prints Name
+ * @name: name of the person
+ * @f: Pointer to Function
  * Return: Always 0.
  */
 void print_name(char *name, void (*f)(char *))
 {
-f(name);
+(*f)(name);
 }
 
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,10 @@
+/**
+ *op_add - check if the absolute value of a number is 98
+ *@elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int op_add(int a, int b)
+{
+
+}
