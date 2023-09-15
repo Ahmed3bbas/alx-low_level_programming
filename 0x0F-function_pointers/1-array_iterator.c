ee306404a14d691c1b2ed61e72753ddf66770d1b
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-09-15 11:06:58 -0700

    Add 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ *array_iterator - Function that Executes Functions.
+ *@array: Address of the Array.
+ *@size: Size of the Array.
+ *@action: Function to be executed.
+ *Return: Always 0.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t counter = 0;
+while (counter < size)
+{
+action(array[counter]);
+counter++;
+}
+}
+
