8b478bea6acff445bea2c93de6499502e83bc593
medoman3333 <eng.ahmedkhaled53@gmail.com>
2023-09-28 14:50:30 -0700

    add: 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,59 @@
 #include "main.h"
 /**
+ * power - power of a number
+ * @n: number
+ * @p: power
+ *
+ * Return: int
+*/
+
+int power(int n, int p)
+{
+	int s = 1, i = 0;
+
+	while (i < p)
+	{
+		s *= n;
+		i++;
+	}
+	return (s);
+}
+
+/**
+ * get_number_of_digits - get number of digits for binary
+ * @n: number
+ * Return: int
+*/
+
+int get_number_of_digits(int n)
+{
+	int i = 0, p = 2;
+
+	while (n > (p - 1))
+	{
+		i++;
+		p = power(2, i);
+	}
+	return (i);
+}
+
+
+/**
  * print_binary - convert decimel to binary
  * @n: int
- * Returns: void
+ * Return: void
 */
 
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
-		_putchar('0');
+	int i, p = get_number_of_digits(n);
 
-	while (n)
+	i = p - 1;
+	if (n <= 1)
+		_putchar(n + '0');
+	while (i >= 0)
 	{
-		_putchar((n % 2) + '0');
-		n /= 2;
+		_putchar(((n >> i) & 1) + '0');
+		i--;
 	}
 }
