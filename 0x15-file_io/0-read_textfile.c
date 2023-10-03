a76d440d85eebc7f3de515b41a725abf16bd17d3
ahmed3bbas <eng.ahmedkhaled53@gmail.com>
2023-10-02 17:22:26 -0700

    update: 0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<unistd.h>
 #include <fcntl.h>
-#include "main.h"
 
 /**
  * read_textfile - read number of letter from file
@@ -12,42 +11,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i = 0, max, err;
+	int err;
 	char buf[1024], c;
 	size_t j, n = 0;
-	size_t nbytes = sizeof(buf);
 	size_t read_bytes;
-
-	if (letters % 1024 > 0)
-	{
-		max = (letters / 1024) + 1;
-	}
-	else
-	{
-		max = (letters / 1024);
-	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
 
-	while (i < max)
+	while ((read_bytes = read(fd, buf, sizeof(buf))) > 0)
 	{
-		read_bytes = read(fd, buf, nbytes);
-		if (read_bytes <= 0)
-			return (0);
-
 		j = 0;
 		while (j < read_bytes && n < letters)
 		{
 			c = buf[j];
-			err = _putchar(c);
+			err = write(1, &c, 1);
 			if (err < 0)
 				return (0);
 			n++;
 			j++;
 		}
-		i++;
 	}
 	close(fd);
 	return (n);
